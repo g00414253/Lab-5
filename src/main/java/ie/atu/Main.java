1963f8a8d07aea0dd13b79872f4fc4bf2a37f0f3
G00414253@atu.ie <g00414253@atu.ie>
2023-10-17 13:38:41 +0100

    Created class

--- src/main/java/ie/atu/Main.java
+++ src/main/java/ie/atu/Main.java
@@ -1,20 +1,41 @@
 package ie.atu;
 
-public class Person() {
-    String firstName = "";
-    String lastName = "";
-    int age = 0;
-}
+public class Person(){
+
+        String firstName = "";
+        String lastName = "";
+        int age = 0;
 
 //Constructor
 public Person(){
-    this.firstName ="";
-    this.lastName ="";
-    this.age;
+        this.firstName ="";
+        this.lastName ="";
+        this.age;
+        }
+
+ public Person(String firstName,String lastName,int age ;){
+        this.firstName=firstName;
+        this.lastName=lastName;
+        this.age=age;
+    }
+
+  public void displayInfo(){
+    System.out.println("Name: " + firstName + "" + lastName + ",Age: " +lastName age);
+  }
+
 }
+
+
 
 
 
 public class Main {
+    public static void main(String[] args){
+        Person johnDoe = new Person("John","Doe",20);
+        Person Blank = new Person();
 
+
+        johnDoe.displayInfo();
+        Blank.displayInfo();
+    }
 }
